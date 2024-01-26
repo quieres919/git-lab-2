23d260b41984cfe0135fba6c37dcc843ad4f64a2
William Marshall <william.marshall919@gmail.com>
2024-01-26 15:25:22 -0500

    Last commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+
+/*
+ *        File: git-lab-program.cc
+ *      Author: <William Marshall>
+ *        Date: <1-26-24>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git !!" << endl;
+    /*add code*/
+    return 0;
+}// main
